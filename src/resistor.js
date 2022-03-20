5a7d4601f34375d85f797a8ead35d2ca0f11a43f
nova557 <71886498+nova557@users.noreply.github.com>
2022-03-20 15:19:15 -0400

    Edit js

--- src/resistor.js
+++ src/resistor.js
@@ -170,12 +170,12 @@ function formatNumber(val) {
     { value: 1e3, symbol: 'k' },
     { value: 1e6, symbol: 'M' },
     { value: 1e9, symbol: 'G' },
-    { value: 1e12, symbol: 'T' },
-    { value: 1e15, symbol: 'P' },
-    { value: 1e18, symbol: 'E' },
   ];
-  const rx = /\.0$|(\.[0-9]*[1-9])0+$/;
-  const item = lookup.slice().reverse().find();
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  const item = lookup
+    .slice()
+    .reverse()
+    .find((element) => val >= element.value);
   return item
     ? (val / item.value).toFixed(2).replace(rx, '$1') + item.symbol
     : '0';
@@ -241,7 +241,7 @@ function getResistorOhms(bands) {
   const val = getThreeBandValue(bands);
   const format = formatNumber(val);
   const tolerance = getTolerance(bands.tolerance);
-  return 'Resistor value: ${format} Ohms ${tolerance}';
+  return `Resistor value: ${format} Ohms ${tolerance}`;
 }
 
 module.exports = {
